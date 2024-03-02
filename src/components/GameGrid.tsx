941f489af3364ab3de3fee910bcd27d424fa8193
leandro-fig <leandrofigueiranav@gmail.com>
2024-03-01 23:05:18 -0300

    Genre list + spinner

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,15 +6,15 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5];
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <div>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 6 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
